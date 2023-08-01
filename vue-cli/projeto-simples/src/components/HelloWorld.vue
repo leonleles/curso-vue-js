5f28ac2ed530e270e8169cbd80fa3b5838b4d566
Leonardo <leonardo.alves-terceiro@cea.com.br>
2023-08-01 18:33:42 -0300

    projeto-simples

---
+++ vue-cli/projeto-simples/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <button @click="onClickButton">Clique aqui</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    onClickButton() {
+      alert(123)
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
