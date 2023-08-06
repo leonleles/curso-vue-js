8843884338fa16c643331aedba3cf39868cb0b38
Leonardo <leonardo.alves-terceiro@cea.com.br>
2023-08-06 14:56:57 -0300

    filtros e mixins

---
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p>{{ usuarioLogado }}</p>
+		<hr>
+		<p>{{cpfDoAluno | cpfMask | inverter}}</p>
+		<input type="text" :value="cpfDoAluno | cpfMask | inverter">
+		<hr>
+
+		<Frutas/>
+
+		<hr>
+		<div>
+        <ul>
+            <li v-for="fruta in frutas" :key="fruta">{{ fruta }}</li>
+        </ul>
+        <input type="text" v-model="fruta" @keydown.enter="add">
+    </div>
+	</div>
+</template>
+
+<script>
+import Frutas from "./Frutas.vue";
+import frutasMixin from "./frutasMixin";
+import usuarioMixin from "./usuarioMixin";
+
+export default {
+  components: { Frutas },
+  mixins: [frutasMixin, usuarioMixin],
+  filters: {
+    cpfMask(valor) {
+      const arr = valor.split("");
+
+      arr.splice(3, 0, ".");
+      arr.splice(7, 0, ".");
+      arr.splice(11, 0, "-");
+
+      return arr.join("");
+    },
+  },
+  data() {
+    return {
+      cpfDoAluno: "50045563311",
+      frutas: ["abacate"],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
+}
+
+input {
+  font-size: 2.5rem;
+}
+</style>
---
+++ filtros-mixins/filtros-mixins-exercicios/src/usuarioMixin.js
@@ -0,0 +1,11 @@
+export default {
+  computed: {
+    usuarioLogado() {
+      return "Maria Silva";
+    },
+  },
+  created() {
+    // eslint-disable-next-line no-console
+    console.log("Created - usuario mixin");
+  },
+};
