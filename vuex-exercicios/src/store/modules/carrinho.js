2882c62b4a06764d20b4e45ba280bf14d2cd59f1
Leonardo <leonardo.alves-terceiro@cea.com.br>
2023-08-08 17:53:52 -0300

    gerenciamento de estado com vuex

---
+++ vuex-exercicios/src/store/modules/carrinho.js
@@ -0,0 +1,34 @@
+export default {
+  namespaced: true,
+  state: {
+    produtos: [],
+  },
+  getters: {
+    valorTotal(state) {
+      return state.produtos
+        .map((p) => p.quantidade * p.preco)
+        .reduce((total, atual) => total + atual, 0);
+    },
+  },
+  mutations: {
+    adicionarProduto(state, payload) {
+      state.produtos.push(payload);
+    },
+  },
+  actions: {
+    adicionarProduto(context, payload) {
+      setTimeout(() => {
+        context.commit("adicionarProduto", payload);
+      }, 1000);
+    },
+    // Exemplo root
+    // adicionarProduto: {
+    //   root: true,
+    //   handler(context, payload) {
+    //     setTimeout(() => {
+    //       context.commit("adicionarProduto", payload);
+    //     }, 1000);
+    //   }
+    // },
+  },
+};
